4a1539dfd0eeff0e9e7b606e02c9de5ef7176ef1
Kalle Norrestam <kalle.norrestam@empear.com>
2022-05-18 11:01:57 +0200

    CS-1208: Use target-ref in api call

--- src/codescene_ci_cd/codescene_api.clj
+++ src/codescene_ci_cd/codescene_api.clj
@@ -2,15 +2,19 @@
   "Wraps the http API to CodeScene delta analysis"
   (:require [clj-http.client :as http]))
 
-(defn run-delta-analysis-on-commits [delta-analysis-url user password repository external-review-id
-                                     coupling-threshold-percent commits timeout]
+(defn run-delta-analysis-on-commits 
+  [delta-analysis-url
+   {:keys [user password 
+           repository external-review-id delta-branch-head target-ref
+           coupling-threshold-percent commits timeout] :as _options}]
   (-> (http/post delta-analysis-url
                  {:basic-auth     [user password]
                   :content-type   :json
                   :form-params    {:commits                    commits
                                    :repository                 repository
                                    :external_review_id         external-review-id
-                                   :delta_branch_head          (last commits)
+                                   :delta_branch_head          delta-branch-head
+                                   :target_ref                 target-ref
                                    :coupling_threshold_percent coupling-threshold-percent}
                   :as             :json
                   :socket-timeout timeout
@@ -20,12 +24,19 @@
 (comment
   (def delta-analysis-url "http://localhost:3003/projects/8/delta-analysis")
   (def user "bot")
-  (def password "")
+  (def password "secret")
   (def repository "analysis-target")
   (def external-review-id "SomeId")
   (def coupling-threshold-percent 45)
-  (def commits ["8eefeb375b1905ef0fbd914c909cffad49d2b6b8"])
+  (def commits ["5ac08b3ef0cb5a882550d9e04b2d74f959639ca3"])
   (def timeout 10000)
-  (def current-commit "8eefeb375b1905ef0fbd914c909cffad49d2b6b8")
-  (run-delta-analysis-on-commits delta-analysis-url user password repository external-review-id
-                                 coupling-threshold-percent commits timeout nil))
+  (run-delta-analysis-on-commits delta-analysis-url 
+                                 {:user user
+                                  :password password
+                                  :repository repository
+                                  :external-review-id external-review-id
+                                  :coupling-threshold-percent coupling-threshold-percent
+                                  :delta-branch-head (last commits)
+                                  :target-ref "a033ea6"
+                                  :commits commits 
+                                  :timeout timeout}))
